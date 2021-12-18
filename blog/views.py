714b2e54b212ef699524db530d37d1faa2e3fcb3
faniry <fanirisoafanomezana@gmail.com>
2021-12-18 20:26:39 +0300

    new post

--- blog/views.py
+++ blog/views.py
@@ -11,10 +11,10 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
-def post_new(request):
+def new_post(request):
     form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
-def post_new(request):
+def new_post(request):
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
